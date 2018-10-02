a6edb84ec3f96881e9ce538a44c8f346501d1f33
D'Angelo Felix <42780331+dfelix4@users.noreply.github.com>
2018-10-02 11:23:34 -0700

    Texture functions

--- dangeloF.cpp
+++ dangeloF.cpp
@@ -2,6 +2,7 @@
 //Date: 9/26/18
 
 #include "fonts.h"
+#include <GL/glx.h>
 #include<iostream>
 using namespace std;
 void creditsD(int x, int y)
@@ -11,4 +12,19 @@ void creditsD(int x, int y)
     r.left = x;
     r.center = 0;
     ggprint16(&r, 16, 0x00ff0000, "D'Angelo Felix");
+}
+void showDPic(int x, int y, GLuint textid)
+{
+ glColor3ub(255,255,255);
+                int wid = 40;
+                glPushMatrix();
+                glTranslatef(x,y,0);
+                glBindTexture(GL_TEXTURE_2D, textid);
+                glBegin(GL_QUADS);
+                glTexCoord2f(0.0f, 1.0f); glVertex2i(-wid,-wid);
+                glTexCoord2f(0.0f, 0.0f); glVertex2i(-wid, wid);
+                glTexCoord2f(1.0f, 0.0f); glVertex2i( wid, wid);
+                glTexCoord2f(1.0f, 1.0f); glVertex2i( wid,-wid);
+                glEnd();
+                glPopMatrix();
 }
