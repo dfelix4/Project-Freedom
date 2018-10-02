3f7c7d4af47ea7cf00100752223231301b3e915d
Student <student@lab-240-025.cs.csubak.edu>
2018-10-02 11:19:10 -0700

    Picture

--- juanO.cpp
+++ juanO.cpp
@@ -18,3 +18,20 @@ void ShowCredits(int x, int y)
      ggprint16(&r, 16, 0x00ff0000, "\nJuan Orozco\n ");
 
 }
+void JuanPicture(int x, int y, GLuint texid)
+{
+    glColor3ub(255,255,255);
+    int wid=30;
+    glPushMatrix();
+    glTranslatef(x, y, 0);
+    glBindTexture(GL_TEXTURE_2D, texid);
+    glBegin(GL_QUADS);
+    glTexCoord2f(0.0f, 1.0f); glVertex2i(-wid,-wid);
+    glTexCoord2f(0.0f, 0.0f); glVertex2i(-wid, wid);
+    glTexCoord2f(1.0f, 0.0f); glVertex2i( wid, wid);
+    glTexCoord2f(1.0f, 1.0f); glVertex2i( wid,-wid);
+
+    glEnd();
+    glPopMatrix();
+
+}
