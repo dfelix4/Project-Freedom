0d7ea5ed4e79936e70512d77459fae644a0a669f
D'Angelo Felix <42780331+dfelix4@users.noreply.github.com>
2018-10-02 11:27:37 -0700

    Added the Picture

--- andresZ.cpp
+++ andresZ.cpp
@@ -3,6 +3,7 @@
 
 #include "fonts.h"
 #include <iostream>
+#include <GL/glx.h>
 using namespace std;
 
 void showAndresName(int x, int y)
@@ -12,4 +13,21 @@ void showAndresName(int x, int y)
 	r.left = x;
 	r.center = 0;
 	ggprint16(&r, 16, 0x00ff0000, "Andres Zamora");
+}
+void showAndresPic(int x, int y, GLuint textid)
+{
+                glColor3ub(255,255,255);
+                int wid = 40;
+                glPushMatrix();
+                glTranslatef(x,y,0);
+                glBindTexture(GL_TEXTURE_2D, textid);
+                //glEnable(GL_ALPHA_TEST);
+                glBegin(GL_QUADS);
+                glTexCoord2f(0.0f, 1.0f); glVertex2i(-wid,-wid);
+                glTexCoord2f(0.0f, 0.0f); glVertex2i(-wid, wid);
+                glTexCoord2f(1.0f, 0.0f); glVertex2i( wid, wid);
+                glTexCoord2f(1.0f, 1.0f); glVertex2i( wid,-wid);
+                glEnd();
+                glPopMatrix();
+
 }
