c0c6d77add700d3c1a62ae3dd5c002a2f6657094
Dangelo Felix <dfelix@odin.cs.csubak.edu>
2018-11-11 22:51:56 -0800

    power up functions

--- dangeloF.cpp
+++ dangeloF.cpp
@@ -4,7 +4,7 @@
 #include "fonts.h"
 #include <GL/glx.h>
 #include <cmath>
-
+#include <stdio.h>
 void creditsD(int x, int y)
 {
     Rect r;
@@ -48,8 +48,46 @@ void test()
 void fireball() 
 {
 }
-void deagle()
+void deagle_time()
 {
+    //a little time between each bullet
+    struct timespec bt;
+    clock_gettime(CLOCK_REALTIME, &bt);
+    double ts = timeDiff(&g.bulletTimer, &bt);
+    if (ts > 0.1) {
+        timeCopy(&g.bulletTimer, &bt);
+        if (g.nbullets < MAX_BULLETS) {
+            //shoot a bullet...
+            //Bullet *b = new Bullet;
+            Bullet *b = &g.barr[g.nbullets];
+            timeCopy(&b->time, &bt);
+            b->pos[0] = g.ship.pos[0];
+            b->pos[1] = g.ship.pos[1];
+            b->vel[0] = g.ship.vel[0];
+            b->vel[1] = g.ship.vel[1];
+            //convert ship angle to radians
+            Flt rad = ((g.ship.angle+90.0) / 360.0f) * PI * 2.0;
+            //convert angle to a vector
+            Flt xdir = cos(rad);
+            Flt ydir = sin(rad);
+            b->pos[0] += xdir*20.0f;
+            b->pos[1] += ydir*20.0f;
+            b->vel[0] += xdir*6.0f + rnd()*0.1;
+            b->vel[1] += ydir*6.0f + rnd()*0.1;
+            b->color[0] = 1.0f;
+            b->color[1] = 1.0f;
+            b->color[2] = 1.0f;
+            g.nbullets++;
+        }
+    }
+}
+void deagle_speed() 
+{
+}
+void deagle_impact() 
+{
+    memcpy(&g.barr[i], &g.barr[g.nbullets-1], sizeof(Bullet));
+    g.nbullets--;
 }
 void shottie () 
 {
